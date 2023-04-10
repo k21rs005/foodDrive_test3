1754bafe4acfd5017f837bd890b3e46c84b52bcb
k21rs005 <113566783+k21rs005@users.noreply.github.com>
2023-04-10 15:30:24 +0900

    金政が途中まで作成したHPを取得した．

---
+++ docs/menu.js
@@ -0,0 +1,10 @@
+$(".openbtn4").click(function () {
+    $('.openbtn4  ,.slide-menu').toggleClass('active');
+});
+
+$('#page-link a[href*="#"]').click(function () {//全てのページ内リンクに適用させたい場合はa[href*="#"]のみでもOK
+	var elmHash = $(this).attr('href'); //ページ内リンクのHTMLタグhrefから、リンクされているエリアidの値を取得
+	var pos = $(elmHash).offset().top;	//idの上部の距離を取得
+	$('body,html').animate({scrollTop: pos}, 500); //取得した位置にスクロール。500の数値が大きくなるほどゆっくりスクロール
+	return false;
+});
