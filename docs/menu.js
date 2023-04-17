97a4bf605caa6c062e00863cc5b0f99d266b7ac8
Takumi KANEMASA <113566763+k21rs034@users.noreply.github.com>
2023-04-17 10:47:51 +0900

    webページのシステムとその配置の変更

--- docs/menu.js
+++ docs/menu.js
@@ -2,9 +2,28 @@ $(".openbtn4").click(function () {
     $('.openbtn4  ,.slide-menu').toggleClass('active');
 });
 
+$(".btn").click(function () {
+    $('.openbtn4  ,.slide-menu').removeClass('active');
+});
+
 $('#page-link a[href*="#"]').click(function () {//全てのページ内リンクに適用させたい場合はa[href*="#"]のみでもOK
 	var elmHash = $(this).attr('href'); //ページ内リンクのHTMLタグhrefから、リンクされているエリアidの値を取得
 	var pos = $(elmHash).offset().top;	//idの上部の距離を取得
 	$('body,html').animate({scrollTop: pos}, 500); //取得した位置にスクロール。500の数値が大きくなるほどゆっくりスクロール
 	return false;
 });
+
+const topPageBtn = document.querySelector('#top-page-btn');
+
+window.addEventListener('scroll', () => {
+  if (window.pageYOffset > 100 &&window.pageYOffset <= 5750) {
+    topPageBtn.style.display = 'block';
+  } else {
+    topPageBtn.style.display = 'none';
+  }
+});
+
+topPageBtn.addEventListener('click', () => {
+  window.scrollTo(0, 0);
+});
+
